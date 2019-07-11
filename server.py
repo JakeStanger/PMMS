1c894e173d0da3433b72158071e13c98573b6825
Jake Stanger <mail@jstanger.dev>
2019-07-11 22:20:34 +0100

    Set secret key

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import logging
+import settings
 
 app: Flask = Flask(__name__)
 logger: logging.Logger
@@ -12,6 +13,9 @@ def hello_world():
 
 def __run__():
     logger.debug('Starting Flask server.')
+
+    app.secret_key = settings.get_key('secret_key')
+
     app.run()
 
 
