0ea97ff7110b24d93a2f368ec8c1b2c2e5e48081
Jake Stanger <mail@jstanger.dev>
2019-07-20 17:51:37 +0100

    Write skeleton watchers

--- plugins/base/movies/watcher.py
+++ plugins/base/movies/watcher.py
@@ -1,2 +1,48 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import logging
+import settings
+import os
+import atexit
+import server
+from database import db
+
+logger: logging.Logger
+observer: Observer
+
+
+class MoviesEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        pass
+
+    def on_moved(self, event):
+        pass
+
+    def on_modified(self, event):
+        pass
+
+    def on_deleted(self, event):
+        pass
+
+
 def watch_movies():
-    pass  # TODO: Write watcher
+    global observer
+    global logger
+
+    logger = logging.getLogger(__name__)
+
+    path = os.path.expanduser(settings.get_key('plugins.base.movies.path'))
+
+    logger.debug('Starting movies filewatcher on \'%s\'' % path)
+
+    event_handler = MoviesEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+
+
+@atexit.register
+def unwatch_movies():
+    logger.debug('Stopping movies filewatcher')
+    observer.stop()
+
--- plugins/base/television/watcher.py
+++ plugins/base/television/watcher.py
@@ -1,2 +1,48 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import logging
+import settings
+import os
+import atexit
+import server
+from database import db
+
+logger: logging.Logger
+observer: Observer
+
+
+class TelevisionEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        pass
+
+    def on_moved(self, event):
+        pass
+
+    def on_modified(self, event):
+        pass
+
+    def on_deleted(self, event):
+        pass
+
+
 def watch_television():
-    pass  # TODO: Write watcher
+    global observer
+    global logger
+
+    logger = logging.getLogger(__name__)
+
+    path = os.path.expanduser(settings.get_key('plugins.base.tv.path'))
+
+    logger.debug('Starting television filewatcher on \'%s\'' % path)
+
+    event_handler = TelevisionEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+
+
+@atexit.register
+def unwatch_television():
+    logger.debug('Stopping television filewatcher')
+    observer.stop()
+
