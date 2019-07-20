a614890b29861c5e11ee08127e7575262a7a2122
Jake Stanger <mail@jstanger.dev>
2019-07-20 16:14:43 +0100

    Add new track watching

--- plugins/base/music/watcher.py
+++ plugins/base/music/watcher.py
@@ -4,6 +4,9 @@ import logging
 import settings
 import os
 import atexit
+import server
+from database import db
+from .scanner import import_track
 
 logger: logging.Logger
 observer: Observer
@@ -21,7 +24,11 @@ class MusicEventHandler(FileSystemEventHandler):
 
         path = event.src_path
 
+        with server.app.app_context():
+            import_track(path, settings.get_key('plugins.base.music.path'))
+            db.session.commit()
 
+        logger.info('Imported new track at \'%s\'' % path)
 
     def on_deleted(self, event):
         logger.debug(event)
