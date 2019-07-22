693a1afddad709bb7721e428d7a2268216f488b2
Jake Stanger <mail@jstanger.dev>
2019-07-22 22:49:33 +0100

    Begin writing base plugin tests

---
+++ plugins/base/tests/test_music.py
@@ -0,0 +1,45 @@
+import server
+import database
+import settings
+import plugin_loader
+from plugins.base.music.models import Artist, Album, Track
+
+settings.__start__()
+server.__start__()
+database.__start__()
+plugin_loader.__start__()
+
+
+def test_import():
+    with server.app.test_client() as c:
+        rv = c.get('/import/music')
+        res = rv.json
+        assert res['message'] == 'Import successful'
+
+
+def test_artist_exists():
+    artist = database.db.session.query(Artist).filter_by(name='Hello').one()
+    assert artist is not None
+    assert artist.name == 'Hello'
+    assert artist.name_sort == 'Hello'
+    assert type(artist.id) == int
+
+
+def test_album_exists():
+    album = database.db.session.query(Album).filter_by(name='World').one()
+    assert album is not None
+    assert album.name == 'World'
+    assert album.name_sort == 'World'
+    assert type(album.id) == int
+
+
+def test_tracks_exist():
+    tracks = database.db.session.query(Track).filter(Track.album.has(name='World')).all()
+    assert tracks is not None
+    assert len(tracks) == 10
+
+    track = tracks[0]
+
+    assert track.name == 'Hello'
+    assert track.name_sort == 'Hello'
+    assert type(track.id) == int
