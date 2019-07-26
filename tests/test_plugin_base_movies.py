44db2b188827f5e40243683ba5efb004103da124
Jake Stanger <mail@jstanger.dev>
2019-07-26 19:34:42 +0100

    Write movie tests

---
+++ tests/test_plugin_base_movies.py
@@ -0,0 +1,30 @@
+import server
+import database
+import settings
+import plugin_loader
+from plugins.base.movies.models import Movie
+
+
+def test_app_start():
+    server.__start__()
+    settings.__start__()
+    database.__start__()
+
+    plugin_loader.__start__()
+
+
+def test_import():
+    with server.app.test_client() as c:
+        rv = c.post('/import/movies')
+        assert rv.status_code == 201
+        res = rv.json
+        assert res['message'] == 'Import successful'
+
+
+def test_movie_exists():
+    movie = database.db.session.query(Movie).filter_by(name='The Code Movie').one()
+    assert movie is not None
+    assert movie.name == 'The Code Movie'
+    assert movie.name_sort == 'Code Movie'
+    assert movie.release_date.year == 2019
+    assert type(movie.id) == int
