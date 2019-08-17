7dc7a23b845316b0f1902d7dae24f2cbfcbffec7
Jake Stanger <mail@jstanger.dev>
2019-08-17 14:42:31 +0100

    Add authorization to tests

--- tests/test_plugin_extra.py
+++ tests/test_plugin_extra.py
@@ -14,11 +14,21 @@ def test_app_start():
     plugin_loader.__start__()
 
 
+api_key: str
+
+
+def test_get_api_key():
+    global api_key
+    with server.app.test_client() as c:
+        rv = c.post('/users/login', data=dict(username='test', password='helloworld'))
+        api_key = rv.json['api_key']
+
+
 def test_local_album_art():
     album = database.db.session.query(Album).filter_by(name='World').one()
 
     with server.app.test_client() as c:
-        rv = c.get('/api/albums/%s/art' % album.id)
+        rv = c.get('/api/albums/%s/art' % album.id, headers={'Authorization': api_key})
         assert rv.status_code == 200
         assert rv.content is not None
         assert mimetypes.guess_type(rv.content) == 'image/jpeg'
@@ -28,7 +38,7 @@ def test_lyrics_cache():
     track = database.db.session.query(Track).filter_by(name='Hello World').one()
 
     with server.app.test_client() as c:
-        rv = c.get('/api/tracks/%s/lyrics' % track.id)
+        rv = c.get('/api/tracks/%s/lyrics' % track.id, headers={'Authorization': api_key})
         assert rv.status_code == 200
 
         lyrics = rv.json
